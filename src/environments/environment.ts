9a9d77ab9e317c48ab46279feee4a922dd7fa031
Sergio Domínguez Gómez <91872827+SergioDominguez15@users.noreply.github.com>
2023-02-23 12:34:11 +0100

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyA_rKef13nYI8mzNbmc5CsU-f_iJgrKc60",
+    authDomain: "inmobiliary-48d0d.firebaseapp.com",
+    databaseURL: "https://inmobiliary-48d0d-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "inmobiliary-48d0d",
+    storageBucket: "inmobiliary-48d0d.appspot.com",
+    messagingSenderId: "509760323783",
+    appId: "1:509760323783:web:d531e02b3e663120a6fee2"
+  }
+};
+
